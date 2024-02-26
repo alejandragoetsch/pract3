25a8207db7f6587bbdea9930174b178042df4abc
Alejandra <alejandra.goetsch@gmail.com>
2024-02-26 17:52:26 +0100

    subido

---
+++ app.js
@@ -0,0 +1,93 @@
+let currentQuestionIndex = 0;
+    let questions = [];
+    let contador=0; 
+    let pregunta=0; 
+
+    const getQuestions = async () => {
+        const response = await fetch('https://opentdb.com/api.php?amount=15&category=9&difficulty=medium&type=multiple');
+        const data = await response.json();
+        questions = data.results;
+        showQuestion();
+        hideWindowContent("app-questions");
+        hideWindowContent("app-end");
+    };
+
+    const hideWindowContent = (id) => {
+        $("#" + id).addClass("hidden");
+    };
+
+    const showWindowContent = (id) => {
+        $("#" + id).removeClass("hidden");
+    };
+
+  $("#start-btn").on("click", (event) => {
+        hideWindowContent("app-entry");
+        hideWindowContent("app-end");
+        showWindowContent("app-questions");
+    }); 
+
+    
+
+    function showQuestion() {
+        
+        const currentQuestion = questions[currentQuestionIndex];
+        const questionElement = document.getElementById('question');
+        questionElement.innerHTML = `
+            <h5 class="card-title">${currentQuestion.question}</h5>
+        `;
+        const optionsContainer = document.getElementById('options');
+        optionsContainer.innerHTML = '';
+        const allOptions = shuffle([...currentQuestion.incorrect_answers, currentQuestion.correct_answer]);
+        allOptions.forEach(option => {
+            const optionButton = document.createElement('button');
+            optionButton.innerText = option;
+            optionButton.style.fontSize = '17px';
+            optionButton.style.margin = '4px';
+            optionButton.onclick = () => checkAnswer(option === currentQuestion.correct_answer, currentQuestion.correct_answer );
+            optionsContainer.appendChild(optionButton);
+        });
+        document.getElementById('contador').innerText = contador; 
+    }
+
+    function checkAnswer(isCorrect, correct_answer) {
+        
+        if (isCorrect) {
+            alert('Correct answer! ✓' ); 
+            contador++; 
+
+        } else {
+            alert('Wrong answer!  ✗ \n The correct answer is :  '+ correct_answer );
+        }
+        currentQuestionIndex++;
+        if (currentQuestionIndex < questions.length) {
+            showQuestion();
+        } else {
+            alert('Trivial Completed! \n Score: '+ contador+'/10');
+            hideWindowContent("app-entry");
+            hideWindowContent("app-questions");
+            showWindowContent("app-end");
+            contador=0; 
+        }
+    }
+
+    $("#Restart-btn").on("click", (event) => {
+        hideWindowContent("app-entry");
+        hideWindowContent("app-end");
+        showWindowContent("app-entry");
+    }); 
+
+    function shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+
+window.onload = getQuestions;
+
+
+
+
+
+
